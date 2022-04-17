c96072b319c5e7ccdee69fa7d46c7d7e151f9266
Esen <yshen.chia@gmail.com>
2022-04-17 22:22:17 +0800

    wip

--- src/koans/11_lazy_sequences.clj
+++ src/koans/11_lazy_sequences.clj
@@ -3,14 +3,14 @@
 
 (meditations
   "There are many ways to generate a sequence"
-  (= __ (range 1 5))
+  (= [1 2 3 4] (range 1 5))
 
   "The range starts at the beginning by default"
-  (= __ (range 5))
+  (= [0 1 2 3 4] (range 5))
 
   "Only take what you need when the sequence is large"
   (= [0 1 2 3 4 5 6 7 8 9]
-     (take __ (range 100)))
+     (take  w (range 100)))
 
   "Or limit results by dropping what you don't need"
   (= [95 96 97 98 99]
